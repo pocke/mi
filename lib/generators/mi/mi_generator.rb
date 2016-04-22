074fcbc450dd823e9477dbbc4ef24c051bd8e264
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-04-23 00:07:00 +0900

    add

--- lib/generators/mi/mi_generator.rb
+++ lib/generators/mi/mi_generator.rb
@@ -4,26 +4,40 @@ class MiGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def doing
-    require 'pry'
-    binding.pry
+    template 'migration.rb.erb', 'db/migrate/hogehoge.rb'
   end
 
 
   private
 
   def arg_groups
-    args = @_initializer[0..1].flatten
+    @arg_groups ||= (
+      args = @_initializer[0..1].flatten
 
-    current = nil
-    res = args.group_by do |a|
-      if %w[+ - %].include? a[0]
-        current
-      else
-        current = a
-        nil
+      current = nil
+      res = args.group_by do |a|
+        if %w[+ - %].include? a[0]
+          current
+        else
+          current = a
+          nil
+        end
       end
-    end
-    res.delete(nil)
-    res
+      res.delete(nil)
+      res
+    )
+  end
+
+  def migration_class_name
+    'TODO'
+  end
+
+  def parse_column(col)
+    name, type, options = *col.split(':')
+    require 'pry'
+    binding.pry
+    kind = name[0]
+    name = name[1..-1]
+    {name: name, type: type, options: options, kind: kind}
   end
 end
