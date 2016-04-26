75fa1c5c0c2d2039beba6859a9ebed23c5422a55
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-04-26 18:15:54 +0900

    test for remove column

--- spec/generators/mi/mi_generator_spec.rb
+++ spec/generators/mi/mi_generator_spec.rb
@@ -37,4 +37,15 @@ describe MiGenerator do
       is_asserted_by{ File.read(last_migration_file).include? 'add_column :users, :email, :string' }
     end
   end
+
+  context 'when remove a column' do
+    let(:arguments){%w[users -email]}
+
+    include_examples 'should_valid_as_a_ruby_script'
+
+    it 'has `remove_column`' do
+      subject
+      is_asserted_by{ File.read(last_migration_file).include? 'remove_column :users, :email' }
+    end
+  end
 end
