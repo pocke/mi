f9ed4c351b48bc88ebeed005fa467f7bc4ea42e9
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-04-30 11:38:56 +0900

    Write specs

--- spec/generators/mi/mi_generator_spec.rb
+++ spec/generators/mi/mi_generator_spec.rb
@@ -36,6 +36,11 @@ describe MiGenerator do
       subject
       is_asserted_by{ File.read(last_migration_file).include? 'add_column :users, :email, :string' }
     end
+
+    it 'has to in filename' do
+      subject
+      is_asserted_by{ last_migration_file.end_with?('to_users.rb') }
+    end
   end
 
   context 'when remove a column' do
@@ -46,6 +51,27 @@ describe MiGenerator do
     it 'has `remove_column`' do
       subject
       is_asserted_by{ File.read(last_migration_file).include? 'remove_column :users, :email' }
+    end
+
+    it 'has from in filename' do
+      subject
+      is_asserted_by{ last_migration_file.end_with?('from_users.rb') }
+    end
+  end
+
+  context 'when change column' do
+    let(:arguments){%w[users %email:string:{null:true}]}
+
+    include_examples 'should_valid_as_a_ruby_script'
+
+    it 'should has change_column' do
+      subject
+      is_asserted_by{ File.read(last_migration_file).include? 'change_column :users, :email, :string, null: true' }
+    end
+
+    it 'has from in filename' do
+      subject
+      is_asserted_by{ last_migration_file.end_with?('of_users.rb') }
     end
   end
 
