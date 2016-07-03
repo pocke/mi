51b9c948e8bb67bc91611886a290724ead813438
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-07-03 20:14:19 +0900

    Base for creation a table

--- lib/generators/mi/create_generator.rb
+++ lib/generators/mi/create_generator.rb
@@ -2,10 +2,44 @@ require 'generators/mi'
 
 module Mi
   module Generators
-    class CreateGenerator < Rails::Generators::Base
+    class CreateGenerator < Base
+      class TypeIsRequired < StandardError; end
+
       source_root File.expand_path('../templates', __FILE__)
 
       def doing
+        migration_template('create.rb.erb', "db/migrate/#{destination}.rb")
+      end
+
+
+      private
+
+
+      # returns t.TYPE :NAME, OPTIONS: true
+      # e.g.) t.string :email
+      def to_method(col)
+        info = parse_column(col)
+        raise TypeIsRequired, "When mi:create, type is required. Please specify type like `#{info[:name]}:TYPE`" unless info[:type]
+
+        res = "t.#{info[:type]} :#{info[:name]}"
+
+        return res unless info[:options]
+        # TODO: DRY
+        res << ", #{info[:options][1..-2].gsub(':', ': ').gsub(',', ', ')}"
+
+        res
+      end
+
+      # when create table, table name is only one.
+      def table_name
+        @table_name ||= (
+          table, = *arg_groups.first
+          table.tableize
+        )
+      end
+
+      def destination
+        "create_#{table_name}_table"
       end
     end
   end
